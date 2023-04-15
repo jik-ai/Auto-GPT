414bd4caf130cc7a7ba9d365e6ed2a903c0bfb98
jik-ai <125036084+jik-ai@users.noreply.github.com>
2023-04-15 09:40:38 +0000

    Fix selenium browsing in dev container

--- autogpt/web.py
+++ autogpt/web.py
@@ -34,6 +34,12 @@ def scrape_text_with_selenium(url):
     options.add_argument(
         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.49 Safari/537.36"
     )
+    options.add_argument("--headless") # Runs Chrome in headley -ss mode.
+    options.add_argument('--no-sandbox') # # Bypass OS security model
+    options.add_argument('start-maximized')
+    options.add_argument('disable-infobars')
+    options.add_argument("--disable-extensions")
+
     driver = webdriver.Chrome(
         executable_path=ChromeDriverManager().install(), options=options
     )
